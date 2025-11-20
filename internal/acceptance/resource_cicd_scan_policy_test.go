48e3d94110fadb4d29d0422c1d473e21fa25d2e7
Schoombee, Jean-Pierre SITI-PTIV/IB/1 <J.Schoombee@shell.com>
2025-11-20 10:34:17 +0100

    feat: complete cicd scanning support

--- internal/acceptance/resource_cicd_scan_policy_test.go
+++ internal/acceptance/resource_cicd_scan_policy_test.go
@@ -3,6 +3,7 @@ package acceptance
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
@@ -84,6 +85,36 @@ func TestAccResourceWizCICDScanPolicyBasic(t *testing.T) {
 						"iac_params.0.custom_ignore_tags.0.rule_ids.0",
 						"063fb380-9eda-4c08-a31b-9211ee37bd42",
 					),
+					// Verify policy_lifecycle_enforcements are set (API defaults)
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.#",
+						"3",
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CLI",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CODE",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "ADMISSION_CONTROLLER",
+							"enforcement_method":   "AUDIT",
+						},
+					),
 				),
 			},
 			{
@@ -108,12 +139,213 @@ func TestAccResourceWizCICDScanPolicyBasic(t *testing.T) {
 	})
 }
 
+func TestAccResourceWizCICDScanPolicyWithPolicyLifecycleEnforcements(t *testing.T) {
+	rName := acctest.RandomWithPrefix(ResourcePrefix)
+	description := "terraform-test with policy lifecycle enforcements"
+	projectID := os.Getenv("WIZ_PROJECT_ID")
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t, TestCase(TcCICDScanPolicy)) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testResourceWizCICDScanPolicyWithPolicyLifecycle(rName, description, projectID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"name",
+						rName,
+					),
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"description",
+						description,
+					),
+					// Verify policy_lifecycle_enforcements are correctly set
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.#",
+						"3",
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CLI",
+							"enforcement_method":   "BLOCK",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CODE",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "ADMISSION_CONTROLLER",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+				),
+			},
+			{
+				// Update to change enforcement methods
+				Config: testResourceWizCICDScanPolicyWithPolicyLifecycleUpdate(rName, "updated-description"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"description",
+						"updated-description",
+					),
+					// Verify updated policy_lifecycle_enforcements
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.#",
+						"3",
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CLI",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CODE",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "ADMISSION_CONTROLLER",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceWizCICDScanPolicyValidation(t *testing.T) {
+	rName := acctest.RandomWithPrefix(ResourcePrefix)
+	projectID := os.Getenv("WIZ_PROJECT_ID")
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t, TestCase(TcCICDScanPolicy)) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      testResourceWizCICDScanPolicyInvalidDeploymentLifecycle(rName, projectID),
+				ExpectError: regexp.MustCompile("expected deployment_lifecycle to be one of"),
+			},
+			{
+				Config:      testResourceWizCICDScanPolicyInvalidEnforcementMethod(rName, projectID),
+				ExpectError: regexp.MustCompile("expected enforcement_method to be one of"),
+			},
+		},
+	})
+}
+
+func TestAccResourceWizCICDScanPolicyWithAdmissionController(t *testing.T) {
+	rName := acctest.RandomWithPrefix(ResourcePrefix)
+	description := "terraform-test with admission controller config"
+	projectID := os.Getenv("WIZ_PROJECT_ID")
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t, TestCase(TcCICDScanPolicy)) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testResourceWizCICDScanPolicyWithAdmissionController(rName, description, projectID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"name",
+						rName,
+					),
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"description",
+						description,
+					),
+					// Verify policy_lifecycle_enforcements are correctly set
+					resource.TestCheckResourceAttr(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.#",
+						"3",
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CLI",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "CODE",
+							"enforcement_method":   "AUDIT",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*",
+						map[string]string{
+							"deployment_lifecycle": "ADMISSION_CONTROLLER",
+							"enforcement_method":   "BLOCK",
+						},
+					),
+					// Verify admission controller config
+					resource.TestCheckTypeSetElemNestedAttrs(
+						resourceWizCICDScanPolicy,
+						"policy_lifecycle_enforcements.*.enforcement_config.*.admission_controller_config.*",
+						map[string]string{
+							"enforce_on_scope": "true",
+						},
+					),
+				),
+			},
+		},
+	})
+}
+
 func testResourceWizCICDScanPolicyBasic(rName string, description string, projectID string) string {
 	return fmt.Sprintf(`
 resource "wiz_cicd_scan_policy" "foo" {
   name        = "%s"
   description = "%s"
   project_ids = [ "%s" ]
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CLI"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CODE"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "ADMISSION_CONTROLLER"
+    enforcement_method   = "AUDIT"
+  }
+
   iac_params {
     count_threshold             = 3
     severity_threshold          = "CRITICAL"
@@ -149,6 +381,22 @@ func testResourceWizCICDScanPolicyUpdate(rName string, description string, count
 resource "wiz_cicd_scan_policy" "foo" {
   name        = "%s"
   description = "%s"
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CLI"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CODE"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "ADMISSION_CONTROLLER"
+    enforcement_method   = "AUDIT"
+  }
+
   iac_params {
     count_threshold             = %d
     severity_threshold          = "CRITICAL"
@@ -177,4 +425,144 @@ resource "wiz_cicd_scan_policy" "foo" {
   }
 }
 `, rName, description, countThreshold)
+}
+
+func testResourceWizCICDScanPolicyWithPolicyLifecycle(rName string, description string, projectID string) string {
+	return fmt.Sprintf(`
+resource "wiz_cicd_scan_policy" "foo" {
+  name        = "%s"
+  description = "%s"
+  project_ids = [ "%s" ]
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CLI"
+    enforcement_method   = "BLOCK"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CODE"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "ADMISSION_CONTROLLER"
+    enforcement_method   = "AUDIT"
+  }
+
+  iac_params {
+    count_threshold             = 3
+    severity_threshold          = "CRITICAL"
+    builtin_ignore_tags_enabled = false
+    ignored_rules = [
+      "fd7dd0c6-4953-4b36-bc39-004ec3d870db",
+    ]
+  }
+}
+`, rName, description, projectID)
+}
+
+func testResourceWizCICDScanPolicyWithPolicyLifecycleUpdate(rName string, description string) string {
+	return fmt.Sprintf(`
+resource "wiz_cicd_scan_policy" "foo" {
+  name        = "%s"
+  description = "%s"
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CLI"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CODE"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "ADMISSION_CONTROLLER"
+    enforcement_method   = "AUDIT"
+  }
+
+  iac_params {
+    count_threshold             = 5
+    severity_threshold          = "HIGH"
+    builtin_ignore_tags_enabled = true
+  }
+}
+`, rName, description)
+}
+
+func testResourceWizCICDScanPolicyInvalidDeploymentLifecycle(rName string, projectID string) string {
+	return fmt.Sprintf(`
+resource "wiz_cicd_scan_policy" "foo" {
+  name        = "%s"
+  description = "test invalid deployment lifecycle"
+  project_ids = [ "%s" ]
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "INVALID_LIFECYCLE"
+    enforcement_method   = "BLOCK"
+  }
+
+  iac_params {
+    count_threshold    = 3
+    severity_threshold = "CRITICAL"
+  }
+}
+`, rName, projectID)
+}
+
+func testResourceWizCICDScanPolicyInvalidEnforcementMethod(rName string, projectID string) string {
+	return fmt.Sprintf(`
+resource "wiz_cicd_scan_policy" "foo" {
+  name        = "%s"
+  description = "test invalid enforcement method"
+  project_ids = [ "%s" ]
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CLI"
+    enforcement_method   = "INVALID_METHOD"
+  }
+
+  iac_params {
+    count_threshold    = 3
+    severity_threshold = "CRITICAL"
+  }
+}
+`, rName, projectID)
+}
+
+func testResourceWizCICDScanPolicyWithAdmissionController(rName string, description string, projectID string) string {
+	return fmt.Sprintf(`
+resource "wiz_cicd_scan_policy" "foo" {
+  name        = "%s"
+  description = "%s"
+  project_ids = [ "%s" ]
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CLI"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "CODE"
+    enforcement_method   = "AUDIT"
+  }
+
+  policy_lifecycle_enforcements {
+    deployment_lifecycle = "ADMISSION_CONTROLLER"
+    enforcement_method   = "BLOCK"
+    enforcement_config {
+      admission_controller_config {
+        enforce_on_scope = true
+      }
+    }
+  }
+
+  iac_params {
+    count_threshold             = 3
+    severity_threshold          = "CRITICAL"
+    builtin_ignore_tags_enabled = false
+  }
+}
+`, rName, description, projectID)
 }
--- internal/provider/resource_cicd_scan_policy_test.go
+++ internal/provider/resource_cicd_scan_policy_test.go
@@ -486,3 +486,451 @@ func TestGetIACParams(t *testing.T) {
 		)
 	}
 }
+
+func TestGetPolicyLifecycleEnforcementsForCreate(t *testing.T) {
+	ctx := context.Background()
+
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name": "test-policy",
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "CLI",
+					"enforcement_method":   "BLOCK",
+				},
+				map[string]interface{}{
+					"deployment_lifecycle": "CODE",
+					"enforcement_method":   "BLOCK",
+				},
+			},
+			"iac_params": []interface{}{
+				map[string]interface{}{
+					"severity_threshold": "CRITICAL",
+					"count_threshold":    3,
+				},
+			},
+		},
+	)
+
+	result := getPolicyLifecycleEnforcementsForCreate(ctx, d)
+
+	// Check that we have exactly 2 items
+	if len(result) != 2 {
+		t.Fatalf("Expected 2 policy lifecycle enforcements, got %d", len(result))
+	}
+
+	// Check that both CLI and CODE enforcements exist with BLOCK method
+	foundCLI := false
+	foundCODE := false
+	for _, enforcement := range result {
+		if enforcement.DeploymentLifecycle == "CLI" && enforcement.EnforcementMethod == "BLOCK" {
+			foundCLI = true
+		}
+		if enforcement.DeploymentLifecycle == "CODE" && enforcement.EnforcementMethod == "BLOCK" {
+			foundCODE = true
+		}
+	}
+
+	if !foundCLI {
+		t.Fatal("Expected CLI enforcement with BLOCK method not found")
+	}
+	if !foundCODE {
+		t.Fatal("Expected CODE enforcement with BLOCK method not found")
+	}
+}
+
+func TestGetPolicyLifecycleEnforcementsForCreateSingle(t *testing.T) {
+	ctx := context.Background()
+
+	expected := []wiz.PolicyLifecycleEnforcementInput{
+		{
+			DeploymentLifecycle: "CLI",
+			EnforcementMethod:   "AUDIT",
+		},
+	}
+
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name": "test-policy-single",
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "CLI",
+					"enforcement_method":   "AUDIT",
+				},
+			},
+			"disk_secrets_params": []interface{}{
+				map[string]interface{}{
+					"count_threshold": 3,
+				},
+			},
+		},
+	)
+
+	result := getPolicyLifecycleEnforcementsForCreate(ctx, d)
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf(
+			expectedTestError,
+			result,
+			expected,
+		)
+	}
+}
+
+func TestFlattenPolicyLifecycleEnforcements(t *testing.T) {
+	ctx := context.Background()
+
+	input := []wiz.PolicyLifecycleEnforcementOutput{
+		{
+			DeploymentLifecycle: "CLI",
+			EnforcementMethod:   "BLOCK",
+		},
+		{
+			DeploymentLifecycle: "CODE",
+			EnforcementMethod:   "AUDIT",
+		},
+	}
+
+	expected := []interface{}{
+		map[string]interface{}{
+			"deployment_lifecycle": "CLI",
+			"enforcement_method":   "BLOCK",
+			"enforcement_config":   []interface{}{},
+		},
+		map[string]interface{}{
+			"deployment_lifecycle": "CODE",
+			"enforcement_method":   "AUDIT",
+			"enforcement_config":   []interface{}{},
+		},
+	}
+
+	result := flattenPolicyLifecycleEnforcements(ctx, input)
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf(
+			expectedTestError,
+			result,
+			expected,
+		)
+	}
+}
+
+func TestFlattenPolicyLifecycleEnforcementsEmpty(t *testing.T) {
+	ctx := context.Background()
+
+	input := []wiz.PolicyLifecycleEnforcementOutput{}
+
+	result := flattenPolicyLifecycleEnforcements(ctx, input)
+
+	// For empty input, result should be an empty slice (length 0)
+	if len(result) != 0 {
+		t.Fatalf("Expected empty slice, got %d items: %v", len(result), result)
+	}
+}
+
+func TestFlattenPolicyLifecycleEnforcementsWithAdmissionController(t *testing.T) {
+	ctx := context.Background()
+
+	enforceOnScope := true
+	input := []wiz.PolicyLifecycleEnforcementOutput{
+		{
+			DeploymentLifecycle: "ADMISSION_CONTROLLER",
+			EnforcementMethod:   "BLOCK",
+			EnforcementConfig: &wiz.PolicyLifecycleEnforcementConfigOutput{
+				EnforceOnScope: &enforceOnScope,
+			},
+		},
+	}
+
+	expected := []interface{}{
+		map[string]interface{}{
+			"deployment_lifecycle": "ADMISSION_CONTROLLER",
+			"enforcement_method":   "BLOCK",
+			"enforcement_config": []interface{}{
+				map[string]interface{}{
+					"admission_controller_config": []interface{}{
+						map[string]interface{}{
+							"enforce_on_scope": true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	result := flattenPolicyLifecycleEnforcements(ctx, input)
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf(
+			expectedTestError,
+			result,
+			expected,
+		)
+	}
+}
+
+func TestHandlePolicyLifecycleEnforcementsUpdate(t *testing.T) {
+	ctx := context.Background()
+
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name": "test-update-policy",
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "CLI",
+					"enforcement_method":   "BLOCK",
+				},
+				map[string]interface{}{
+					"deployment_lifecycle": "CODE",
+					"enforcement_method":   "AUDIT",
+				},
+			},
+			"disk_vulnerabilities_params": []interface{}{
+				map[string]interface{}{
+					"severity":                "HIGH",
+					"package_count_threshold": 5,
+					"ignore_unfixed":          true,
+					"package_allow_list":      []interface{}{},
+				},
+			},
+		},
+	)
+
+	result := handlePolicyLifecycleEnforcementsUpdate(ctx, d)
+
+	// Check that we have exactly 2 items
+	if len(result) != 2 {
+		t.Fatalf("Expected 2 policy lifecycle enforcements, got %d", len(result))
+	}
+
+	// Check that both CLI and CODE enforcements exist with correct methods
+	foundCLI := false
+	foundCODE := false
+	for _, enforcement := range result {
+		if enforcement.DeploymentLifecycle == "CLI" && enforcement.EnforcementMethod == "BLOCK" {
+			foundCLI = true
+		}
+		if enforcement.DeploymentLifecycle == "CODE" && enforcement.EnforcementMethod == "AUDIT" {
+			foundCODE = true
+		}
+	}
+
+	if !foundCLI {
+		t.Fatal("Expected CLI enforcement with BLOCK method not found")
+	}
+	if !foundCODE {
+		t.Fatal("Expected CODE enforcement with AUDIT method not found")
+	}
+}
+
+func TestGetPolicyLifecycleEnforcementsForCreateWithAdmissionController(t *testing.T) {
+	ctx := context.Background()
+
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name": "test-admission-controller-policy",
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "ADMISSION_CONTROLLER",
+					"enforcement_method":   "BLOCK",
+					"enforcement_config": []interface{}{
+						map[string]interface{}{
+							"admission_controller_config": []interface{}{
+								map[string]interface{}{
+									"enforce_on_scope": true,
+								},
+							},
+						},
+					},
+				},
+			},
+			"iac_params": []interface{}{
+				map[string]interface{}{
+					"severity_threshold": "CRITICAL",
+					"count_threshold":    3,
+				},
+			},
+		},
+	)
+
+	result := getPolicyLifecycleEnforcementsForCreate(ctx, d)
+
+	// Check that we have exactly 1 item
+	if len(result) != 1 {
+		t.Fatalf("Expected 1 policy lifecycle enforcement, got %d", len(result))
+	}
+
+	enforcement := result[0]
+
+	// Check basic fields
+	if enforcement.DeploymentLifecycle != "ADMISSION_CONTROLLER" {
+		t.Fatalf("Expected deployment lifecycle ADMISSION_CONTROLLER, got %s", enforcement.DeploymentLifecycle)
+	}
+	if enforcement.EnforcementMethod != "BLOCK" {
+		t.Fatalf("Expected enforcement method BLOCK, got %s", enforcement.EnforcementMethod)
+	}
+
+	// Check enforcement config
+	if enforcement.EnforcementConfig == nil {
+		t.Fatal("Expected enforcement config to be set")
+	}
+	if enforcement.EnforcementConfig.AdmissionControllerConfig == nil {
+		t.Fatal("Expected admission controller config to be set")
+	}
+	if !enforcement.EnforcementConfig.AdmissionControllerConfig.EnforceOnScope {
+		t.Fatal("Expected enforce on scope to be true")
+	}
+}
+
+// Integration tests based on real-world examples
+func TestCICDScanPolicyIntegrationIAC(t *testing.T) {
+	ctx := context.Background()
+
+	// Test IAC scan policy similar to the terraform-test-iac example
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name":        "terraform-test-iac",
+			"description": "terraform-test-iac description",
+			"project_ids": []interface{}{},
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "CLI",
+					"enforcement_method":   "BLOCK",
+				},
+				map[string]interface{}{
+					"deployment_lifecycle": "CODE",
+					"enforcement_method":   "BLOCK",
+				},
+			},
+			"iac_params": []interface{}{
+				map[string]interface{}{
+					"count_threshold":     3,
+					"severity_threshold":  "CRITICAL",
+					"ignored_rules":       []interface{}{},
+					"security_frameworks": []interface{}{},
+				},
+			},
+		},
+	)
+
+	// Test the create functions work together
+	enforcements := getPolicyLifecycleEnforcementsForCreate(ctx, d)
+	iacParams := getIACParamsForCreate(ctx, d)
+
+	// Verify enforcements
+	if len(enforcements) != 2 {
+		t.Fatalf("Expected 2 policy lifecycle enforcements, got %d", len(enforcements))
+	}
+
+	// Verify IAC params
+	if iacParams.CountThreshold != 3 {
+		t.Fatalf("Expected count threshold 3, got %d", iacParams.CountThreshold)
+	}
+	if iacParams.SeverityThreshold != "CRITICAL" {
+		t.Fatalf("Expected severity threshold CRITICAL, got %s", iacParams.SeverityThreshold)
+	}
+}
+
+func TestCICDScanPolicyIntegrationSecrets(t *testing.T) {
+	ctx := context.Background()
+
+	// Test Secrets scan policy similar to the terraform-test-secrets example
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name":        "terraform-test-secrets",
+			"description": "terraform-test-secrets description",
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "CLI",
+					"enforcement_method":   "BLOCK",
+				},
+			},
+			"disk_secrets_params": []interface{}{
+				map[string]interface{}{
+					"count_threshold": 3,
+					"path_allow_list": []interface{}{},
+				},
+			},
+		},
+	)
+
+	// Test the create functions work together
+	enforcements := getPolicyLifecycleEnforcementsForCreate(ctx, d)
+	secretsParams := getDiskSecretsParams(ctx, d)
+
+	// Verify enforcements - only CLI should be present
+	if len(enforcements) != 1 {
+		t.Fatalf("Expected 1 policy lifecycle enforcement, got %d", len(enforcements))
+	}
+	if enforcements[0].DeploymentLifecycle != "CLI" || enforcements[0].EnforcementMethod != "BLOCK" {
+		t.Fatalf("Expected CLI/BLOCK enforcement, got %s/%s", enforcements[0].DeploymentLifecycle, enforcements[0].EnforcementMethod)
+	}
+
+	// Verify secrets params
+	if secretsParams.CountThreshold != 3 {
+		t.Fatalf("Expected count threshold 3, got %d", secretsParams.CountThreshold)
+	}
+}
+
+func TestCICDScanPolicyIntegrationVulnerabilities(t *testing.T) {
+	ctx := context.Background()
+
+	// Test Vulnerabilities scan policy similar to the terraform-test-vulnerabilities example
+	d := schema.TestResourceDataRaw(
+		t,
+		resourceWizCICDScanPolicy().Schema,
+		map[string]interface{}{
+			"name":        "terraform-test-vulnerabilities",
+			"description": "terraform-test-vulnerabilities description",
+			"policy_lifecycle_enforcements": []interface{}{
+				map[string]interface{}{
+					"deployment_lifecycle": "CLI",
+					"enforcement_method":   "BLOCK",
+				},
+			},
+			"disk_vulnerabilities_params": []interface{}{
+				map[string]interface{}{
+					"package_allow_list":      []interface{}{},
+					"package_count_threshold": 3,
+					"severity":                "HIGH",
+					"ignore_unfixed":          false,
+				},
+			},
+		},
+	)
+
+	// Test the create functions work together
+	enforcements := getPolicyLifecycleEnforcementsForCreate(ctx, d)
+	vulnParams := getDiskVulnerabilitiesParams(ctx, d)
+
+	// Verify enforcements - only CLI should be present
+	if len(enforcements) != 1 {
+		t.Fatalf("Expected 1 policy lifecycle enforcement, got %d", len(enforcements))
+	}
+	if enforcements[0].DeploymentLifecycle != "CLI" || enforcements[0].EnforcementMethod != "BLOCK" {
+		t.Fatalf("Expected CLI/BLOCK enforcement, got %s/%s", enforcements[0].DeploymentLifecycle, enforcements[0].EnforcementMethod)
+	}
+
+	// Verify vulnerability params
+	if vulnParams.PackageCountThreshold != 3 {
+		t.Fatalf("Expected package count threshold 3, got %d", vulnParams.PackageCountThreshold)
+	}
+	if vulnParams.Severity != "HIGH" {
+		t.Fatalf("Expected severity HIGH, got %s", vulnParams.Severity)
+	}
+	if vulnParams.IgnoreUnfixed != false {
+		t.Fatalf("Expected ignore_unfixed false, got %t", vulnParams.IgnoreUnfixed)
+	}
+}
